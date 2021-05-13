7c1034b84474dcf7f810397157c3011d8ad66e15
Dylan <dylansomeren@gmail.com>
2021-05-13 19:50:02 +0200

    eerste commit, niks werkt nog

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+//mapbox api
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybHVjaTIiLCJhIjoiY2tvazkyaXptMDM5ZTJwcm1sZW1lcGxoNyJ9.fJvwUDAUZ6zpISneIJ1GgQ';
+var map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', 
+    center: [5.508852, 52.142480], 
+    zoom: 9 
+});
+
+var lat;
+var long;
+
+var searchLocation = new MapboxGeocoder({ 
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+
+    getItemValue: e => {
+        long = e['center'][0];
+        lat = e['center'][1];
+        document.getElementsByClassName('mapboxgl-ctrl-geocoder--input').value = e['place_name']; //lat
+        return e['place_name'];
+    }
+});
+
+
+//Weatherbit api voor herkennen weercondities(op advies van een medestudent deze gebruikt ipv de gebruikte api in de lessen)
+
