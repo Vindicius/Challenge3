48d746f06a7ef9d4fd64b98dac4e42df9a4048b7
Dylan <dylansomeren@gmail.com>
2021-05-13 21:59:21 +0200

    HOLY FCK, het mapbox deel werkt nu
    WHOOOOOOOOOOOOOO

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,7 @@
 //mapbox api
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybHVjaTIiLCJhIjoiY2tvazkyaXptMDM5ZTJwcm1sZW1lcGxoNyJ9.fJvwUDAUZ6zpISneIJ1GgQ';
 var map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', 
-    center: [5.508852, 52.142480], 
-    zoom: 9 
-});
+    container: 'map', style: 'mapbox://styles/mapbox/streets-v11', center: [5, 52], zoom: 9});
 
 var lat;
 var long;
@@ -17,15 +13,16 @@ var searchLocation = new MapboxGeocoder({
     getItemValue: e => {
         long = e['center'][0];
         lat = e['center'][1];
-        document.getElementsByClassName('mapboxgl-ctrl-geocoder--input').value = e['place_name']; //lat
+        document.getElementsByClassName('mapboxgl-ctrl-geocoder--input').value = e['place_name']; 
         return e['place_name'];
     }
 });
 
-
 //Weatherbit api voor herkennen weercondities(op advies van een medestudent deze gebruikt ipv de gebruikte api in de lessen)
 
-const key = '09a75e1975e749bd9d44b0eb6d7dbf5c'; //Key voor API weather
-var temp = document.getElementById('showWeatherTemp');
-var weatherIcon = document.getElementById('weatherIcon');
-weatherIcon.src = 'images/errorIcon.svg'; // default img
+var token = '09a75e1975e749bd9d44b0eb6d7dbf5c'; //Sleutel voor de weer api
+var temp = document.getElementById('showTemp');
+var weather = document.getElementById('weather');
+weather.src = 'img/error.png'; // default img
+
+document.getElementById('search').appendChild(searchLocation.onAdd(map));//zoekbalk
